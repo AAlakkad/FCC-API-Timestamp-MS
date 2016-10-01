8b7ea3fb25dcf4b5623d756cf9c7aa44d648b693
Ammar Alakkad <ammar@Ammars-MacBook-Pro.local>
2016-10-01 10:28:45 +0300

    Add application logic

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,8 +9,32 @@
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
 |
-*/
+ */
 
 $app->get('/', function () use ($app) {
     return $app->version();
+});
+
+$app->get('/{timestamp}', function ($timestamp = null) use ($app) {
+
+    if (is_numeric($timestamp)) {
+        // send the timestamp as human readable text
+        $json = [
+            'unix'    => $timestamp,
+            'natural' => date('F d, Y', $timestamp),
+        ];
+    } else {
+        // send the equivilent unix timestamp
+        $unix = strtotime(urldecode($timestamp));
+        if ($unix === false) {
+            return 'Bad request';
+        }
+
+        $json = [
+            'unix'    => $unix,
+            'natural' => urldecode($timestamp),
+        ];
+    }
+
+    return $json;
 });
